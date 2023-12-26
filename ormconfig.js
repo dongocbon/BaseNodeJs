27d8f04e6302facce44092d8206f3ae63865836d
bondn <bondn@kaopiz.com>
2023-12-26 10:36:12 +0700

    base node

---
+++ ormconfig.js
@@ -0,0 +1,20 @@
+const rootDir = process.env.ORM_ENV == 'orm' ? 'src' : 'build/src';
+
+module.exports = {
+type: process.env.DB_CONNECTION,
+host: process.env.DB_HOST,
+port: process.env.DB_PORT,
+username: process.env.DB_USER,
+password: process.env.DB_PASS,
+database: process.env.DB_NAME,
+synchronize: false,
+logging: false,
+entities: [rootDir + '/entity/index.{js,ts}'],
+migrations: [rootDir + '/migration/**/*.{js,ts}'],
+subscribers: [rootDir + '/subscriber/**/*.{js,ts}'],
+cli: {
+entitiesDir: 'src/entity',
+migrationsDir: 'src/migration',
+subscribersDir: 'src/subscriber',
+},
+};
